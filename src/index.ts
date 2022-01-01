d7445aa365289cd51ce2bd880fbbc84d439078cd
Jonas Dorfinger <contact@dorfingerjonas.at>
2022-01-02 00:11:56 +0100

    implement index.ts

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Dropbox } from 'dropbox';
+import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+
+dotenv.config();
+
+const dropboxClient = new Dropbox({
+  accessToken: process.env['DROPBOX_ACCESS_TOKEN']
+});
+
+app.get('/', (req, res) => {
+  res.send('please use /:path');
+});
+
+app.get('/:path', (req, res) => {
+  dropboxClient.fileRequestsCreate({
+    title: req.params.path,
+    destination: `/${ process.env['FILE_REQUEST_PATH'] }/${ req.params.path }`,
+    open: true
+  })
+    .then(response => {
+      console.log(`Created file-request "${ response.result.title}" (${ response.result.created })`);
+      res.redirect(response.result.url);
+    })
+    .catch(error => {
+      res.send({ error: error.message });
+    });
+});
+
+app.listen(process.env['PORT'], () => {
+  console.log(`Listening on port ${ process.env['PORT'] }`);
+});
